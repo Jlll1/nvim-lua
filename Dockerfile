b35723129c153cb5e5a5dd844ba1417984a21cd7
Jlll1 <arghantentua@tutanota.com>
2022-10-09 01:30:15 +0200

    Add treesitter

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,33 @@ FROM alpine:latest
 
 ENV XDG_DATA_HOME /root/.config/nvim
 
-RUN apk add --update \
+RUN apk add --repository=http://dl-cdn.alpinelinux.org/alpine/edge/community --update \
   neovim \
-  neovim-doc \
+  neovim-doc
+
 # d: nvim-fzf {
+RUN apk add \
   bat \
   fd \
   fzf \
   ripgrep
 # }
 
+# d: treesitter {
+RUN apk add \
+  git \
+  clang \
+  clang-static \
+  clang-dev \
+  build-base \
+  llvm-static \
+  llvm-dev
+# }
+
 WORKDIR /root/.config/nvim
 COPY dot-nvim .
+
+RUN nvim --headless +TSUpdate +qa!
 
 VOLUME /ws
 WORKDIR /ws
--- dot-nvim/init.lua
+++ dot-nvim/init.lua
@@ -30,11 +30,28 @@ opt.expandtab      = true
 opt.tabstop        = 2
 opt.shiftwidth     = 2
 
+-- [[ Colorscheme ]]
+local colorscheme = 'monochromenote'
+pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 
-local colorscheme = "monochromenote"
-pcall(vim.cmd, "colorscheme " .. colorscheme)
+-- [[ Treesitter ]]
+-- Don't pull parsers from github, instead use local submodules
+  local pcfg = require('nvim-treesitter.parsers').get_parser_configs()
+  pcfg.c.install_info.url           = '/root/.config/nvim/treesitter-parsers/tree-sitter-c'
+  pcfg.cpp.install_info.url         = '/root/.config/nvim/treesitter-parsers/tree-sitter-cpp'
+  pcfg.c_sharp.install_info.url     = '/root/.config/nvim/treesitter-parsers/tree-sitter-c-sharp'
+  pcfg.go.install_info.url          = '/root/.config/nvim/treesitter-parsers/tree-sitter-go'
+  pcfg.html.install_info.url        = '/root/.config/nvim/treesitter-parsers/tree-sitter-html'
+  pcfg.lua.install_info.url         = '/root/.config/nvim/treesitter-parsers/tree-sitter-lua'
 
+require('nvim-treesitter.configs').setup({
+  ensure_installed = { 'c', 'cpp', 'c_sharp', 'go', 'html', 'lua' },
+  sync_install = true,
+  auto_install = false,
+  highlight = { enable = true },
+})
 
+-- [[ Keymaps ]]
 keymap.set('n', 'fp',  '<cmd>lua require("commands").files("fd --color always -t f -L")<cr>', { noremap = true, silent = true })
 
 vim.api.nvim_create_user_command(
