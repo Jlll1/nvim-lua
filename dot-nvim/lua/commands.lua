540b1fab4980bba93d309f2b5583db5fb02d7702
Jlll1 <arghantentua@tutanota.com>
2022-10-08 15:11:51 +0200

    Fix file preview in grep

--- dot-nvim/lua/commands.lua
+++ dot-nvim/lua/commands.lua
@@ -1,11 +1,11 @@
 local M = {}
 
 local fzf = require('fzf').fzf
-
-local preview = 'ash -c ' .. vim.fn.shellescape('bat --color never -- \"$0\"') .. ' {}'
+local action = require('fzf.actions').action
 
 local function files(command)
   coroutine.wrap(function ()
+    local preview = 'ash -c ' .. vim.fn.shellescape('bat --color never -- \"$0\"') .. ' {}'
     local choices = fzf(command,
       ("--ansi --preview=%s --expect=ctrl-v,ctrl-h"):format(vim.fn.shellescape(preview)))
     if not choices then return end
@@ -23,11 +23,26 @@ local function files(command)
   end)()
 end
 
+
 local function grep(pattern)
+  local preview_action = action(function (lines, fzf_lines)
+    local filename, row = string.match(lines[1], '(.-):(%d+):.*')
+
+    fzf_lines = tonumber(fzf_lines)
+    local line_start = math.floor(row - (fzf_lines / 2))
+    if line_start < 1 then line_start = 1 end
+    local line_end = math.floor(row + (fzf_lines / 2)) - 1
+
+    local cmd = "bat --style=numbers --color always " .. vim.fn.shellescape(filename) ..
+      " --highlight-line " .. tostring(row) ..
+      " --line-range " .. tostring(line_start) .. ":" .. tostring(line_end)
+    return vim.fn.system(cmd)
+  end)
+
   coroutine.wrap(function ()
-    local rgcmd = 'rg --vimgrep --no-heading --color ansi ' .. vim.fn.shellescape(pattern)
-    local choices = fzf(rgcmd,
-      ("--ansi --preview=%s --expect=ctrl-v,ctrl-h"):format(vim.fn.shellescape(preview)))
+    local rgcmd = "rg --vimgrep --no-heading " ..
+      "--color ansi " ..vim.fn.shellescape(pattern)
+    local choices = fzf(rgcmd, "--ansi --expect=ctrl-v,ctrl-h " .. "--preview " .. preview_action)
     if not choices then return end
 
     local vimcmd
@@ -39,8 +54,8 @@ local function grep(pattern)
       vimcmd = "e"
     end
 
-    local filename = string.match(choices[2], '(.-):.*')
-    vim.cmd(vimcmd .. " " .. vim.fn.fnameescape(filename))
+    local filename = string.match(choices[2], "(.-):")
+    vim.cmd(vimcmd .. " ".. vim.fn.fnameescape(filename))
   end)()
 end
 
