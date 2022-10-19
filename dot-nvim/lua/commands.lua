b039bac493b358e498cf1e67fec092aa7b795d3a
Jlll1 <arghantentua@tutanota.com>
2022-10-19 19:54:28 +0200

    Decl: Handle methods

--- dot-nvim/lua/commands.lua
+++ dot-nvim/lua/commands.lua
@@ -83,27 +83,45 @@ local ts_utils = require('nvim-treesitter.ts_utils')
 local function go_to_declaration()
   -- @INCOMPLETE This isn't exactly right, since we're not verifying whether the cursor is on the
   -- indexed indentifier or on the method identifier
+  -- @IMPROVEMENT Reimplement get_node_at_cursor to remove treesiter-nvim dependency
   local selected_node = ts_utils.get_node_at_cursor()
   local selected_node_text = vim.treesitter.query.get_node_text(selected_node, vim.api.nvim_get_current_buf())
+
+  -- @TODO is there a better way to do this?
+  -- @INCOMPLETE Match more languages
+  -- Convert file_extension to treesitter language
+  local language = vim.bo.filetype
+  if language == 'cs' then
+    language = 'c_sharp'
+  end
+
+  local results = {}
   local rgcmd = "rg --vimgrep --no-heading " .. vim.fn.shellescape(selected_node_text)
   for line in io.popen(rgcmd):lines() do
     local filename = string.match(line, '(.-):.*')
+
+    -- The declaration must be in a language that matches the current one
+    local file_extension = string.match(filename, '.*%.(.*)')
+    if file_extension ~= vim.bo.filetype then
+      goto continue
+    end
+
     local file = io.open(filename, "r")
     local file_content = file:read("*all")
     file:close()
 
-    local results = {}
-    -- @INCOMPLETE handle multiple languages
-    local parser = vim.treesitter.get_string_parser(file_content, "c_sharp")
+    local parser = vim.treesitter.get_string_parser(file_content, language)
     parser:parse()
+
     parser:for_each_tree(function (tstree, tree)
       local root = tstree:root()
 
-      -- @INCOMPLETE this works only for method_declaration, handle function_declaration
-      local query = vim.treesitter.parse_query("c_sharp", "(method_declaration (identifier) @method)")
+      local query = vim.treesitter.parse_query(language, '([(function_declaration (identifier) @target) (method_declaration (field_identifier) @target)])')
       local matches = query:iter_captures(root, file_content, 0, -1)
       for id, node, metadata in matches do
         local node_text = vim.treesitter.query.get_node_text(node, file_content)
+        -- @INCOMPLETE Currently in cases where one file shows more than once (e. g. some method name contains the matching name)
+        -- We get duplicated results, resulting in some methods being listed even though they don't match 100%
         if node_text == selected_node_text then
           results[#results + 1] = line
         end
@@ -133,6 +151,8 @@ local function go_to_declaration()
         vim.api.nvim_win_set_cursor(0, { tonumber(row), tonumber(col) - 1 })
       end)()
     end
+
+    ::continue::
   end
 end
 
